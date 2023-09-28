00e8d0c917d93f5a08dadaee79b74e1ce946fe70
Brooks Benson <brooks@pdrutah.com>
2023-09-27 20:05:25 -0600

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        Testing Vercel
         {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
       </main>
