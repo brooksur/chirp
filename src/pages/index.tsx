296050dbb05628ceb4247c526d65e04ec3df7d82
Brooks Benson <brooks@pdrutah.com>
2023-09-27 20:03:30 -0600

    added vercel.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import Head from "next/head";
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
-import Link from "next/link";
-
-import { api } from "~/utils/api";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
   return (
     <>
